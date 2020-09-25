692afe4cd7a19cfaef49d34f7f374f3d931ac251
liimee <70614549+liimee@users.noreply.github.com>
2020-09-25 22:43:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ var core = {
     document.querySelector('#overlay').style.opacity = 0.85;
     document.querySelector('#home').style.transform = 'scale(0, 0)';
     $('#home').attr('onclick', 'core.closeHome();');
+    core.homeOpen = true;
     setTimeout(function() {
       $('#home').attr('class', 'bx bx-x');
       document.querySelector('#home').style.transform = 'scale(1, 1)';
     }, 500);
   },
   closeHome: function() {
+    core.homeOpen = false;
     document.querySelector('#home').removeAttribute('onclick');
     document.querySelector('#overlay').style.opacity = 0;
     document.querySelector('#home').style.transform = 'scale(0, 0)';
