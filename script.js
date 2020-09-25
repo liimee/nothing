6885759bed6c5116f2ad16376bc352476117f761
liimee <70614549+liimee@users.noreply.github.com>
2020-09-25 17:59:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var core = {
+  root: document.documentElement,
+  openHome: function() {
+    document.querySelector('#overlay').style.opacity = 0.85;
+    document.querySelector('#home').style.transform = 'scale(0, 0)';
+    setTimeout(function())
+    document.querySelector('#home').style.transform = 'scale(1, 1)';
+    $('#home').attr('onclick', 'core.closeHome();');
+  },
+  closeHome: function() {
+    document.querySelector('#overlay').style.opacity = 0;
+    $('#home').attr('onclick', 'core.openHome();');
+  }
+};
+
+if(localStorage.getItem('wp') === null) {
+  localStorage.setItem('wp', 'd-1');
+  core.root.style.setProperty('--bg-img', "url('images/wallpaper-1.jpeg')");
+} else {
+  switch(localStorage.getItem('wp')) {
+    case 'd-1':
+    core.root.style.setProperty('--bg-img', "url('images/wallpaper-1.jpeg')");
+  }
+}
+
+if(localStorage.getItem('wprepeat') === null) {
+  localStorage.setItem('wprepeat', 'false');
+  core.root.style.setProperty('--bg-repeat', 'no-repeat');
+} else {
+  switch(localStorage.getItem('wprepeat')) {
+    case 'false':
+    core.root.style.setProperty('--bg-repeat', 'no-repeat');
+  }
+}
+
+if(localStorage.getItem('dm') === null || localStorage.getItem('dm') == 'false') {
+  localStorage.setItem('dm', 'false');
+  core.root.style.setProperty('--bar-bg', '#fff');
+  core.root.style.setProperty('--text-color', 'black');
+} else {
+  core.root.style.setProperty('--bar-bg', '#212121');
+  core.root.style.setProperty('--text-color', 'white');
+}
