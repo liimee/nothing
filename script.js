a21d4b76478d2d6b5578a1fc8dbc480f659d29aa
liimee <70614549+liimee@users.noreply.github.com>
2020-09-26 20:25:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ var core = {
   },
   openApp: function(app) {
     core.closeHome();
-    $('#desktop').append('<div class="window" id="window-' + core.numberOfWindows + '"style="width: 300px; top: 75px; left: 85px;"><div class="top"><i class="bx bx-x" onclick="core.closeWindow(this);" style="margin-right: 6px; background-color: #ff0000; color: white; border-radius: 30px; cursor: pointer;"></i>Test</div><iframe src="' + app + '"></iframe></div>');
+    $('#desktop').append('<div class="window" id="window-' + core.numberOfWindows + '"style="width: 300px; top: 75px; left: 85px;" onclick="core.focusWindow(this);"><div class="top"><i class="bx bx-x" onclick="core.closeWindow(this);" style="margin-right: 6px; background-color: #ff0000; color: white; border-radius: 30px; cursor: pointer;"></i>Test</div><iframe src="' + app + '"></iframe></div>');
     let position = { x: 0, y: 0 }
 
     interact('.window').draggable({
@@ -73,6 +73,9 @@ var core = {
   },
   closeWindow: function(el) {
     $(el).parent().parent().remove();
+  },
+  focusWindow: function(el) {
+    let elid = el.id.replace('window-', '');
   }
 };
 
