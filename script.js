cfcf92267c58d0f0f2b5315bc743f174ec8e90a8
liimee <70614549+liimee@users.noreply.github.com>
2020-09-26 08:46:34 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var core = {
   homeOpen: false,
   openHome: function() {
     document.querySelector('#home').removeAttribute('onclick');
+    document.querySelector('#overlay').style.zIndex = 4;
     document.querySelector('#overlay').style.opacity = 0.85;
     document.querySelector('#home').style.transform = 'scale(0, 0)';
     $('#home').attr('onclick', 'core.closeHome();');
@@ -16,6 +17,7 @@ var core = {
     core.homeOpen = false;
     document.querySelector('#home').removeAttribute('onclick');
     document.querySelector('#overlay').style.opacity = 0;
+    document.querySelector('#overlay').style.zIndex = 0;
     document.querySelector('#home').style.transform = 'scale(0, 0)';
     $('#home').attr('onclick', 'core.openHome();');
     setTimeout(function() {
@@ -25,7 +27,49 @@ var core = {
   },
   openApp: function(app) {
     core.closeHome();
-    $('#desktop').append('<div class="window" style="top: 70px;"><div class="top">Test</div><iframe src="' + app + '"></iframe></div>');
+    $('#desktop').append('<div class="window" style="top: 70px; width: 100px;"><div class="top"><i class="bx bx-x" onclick="core.closeWindow(this);"></i>Test</div><iframe src="' + app + '"></iframe></div>');
+
+    let position = { x: 0, y: 0 }
+
+    interact('.window').draggable({
+      allowFrom: '.top',
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: 'parent',
+          endOnly: true
+        })
+      ],
+      listeners: {
+        move (event) {
+          position.x += event.dx
+          position.y += event.dy
+
+          event.target.style.transform =
+            `translate(${position.x}px, ${position.y}px)`
+        },
+      }
+    })
+
+    interact('.window').resizable({
+      edges: {
+        top   : false,
+        left  : true,
+        bottom: true,
+        right : true
+      }
+    }).on('resizemove', event => {
+      let { x, y } = event.target.dataset;
+
+      Object.assign(event.target.style, {
+        width: `${event.rect.width}px`,
+        height: `${event.rect.height}px`
+      })
+
+      Object.assign(event.target.dataset, { x, y })
+    })
+  },
+  closeWindow: function(el) {
+    $(el).parent().parent().remove();
   }
 };
 
@@ -57,10 +101,3 @@ if(localStorage.getItem('dm') === null || localStorage.getItem('dm') == 'false')
   core.root.style.setProperty('--bar-bg', '#212121');
   core.root.style.setProperty('--text-color', 'white');
 }
-
-document.addEventListener('DOMContentLoaded', function(){
-new Draggable.Draggable(document.querySelector('#desktop'), {
-  handle: '.top',
-  draggable: '.window'
-});
-});
