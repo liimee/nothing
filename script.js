210e8103ecf2275963cba36054d9ed0315101af2
liimee <70614549+liimee@users.noreply.github.com>
2020-09-26 21:14:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ var core = {
   },
   openApp: function(app) {
     core.closeHome();
-    $('#desktop').append('<div class="window" id="window-' + core.numberOfWindows + '"style="width: 300px; top: 75px; left: 85px;" onclick="core.focusWindow(this);"><div class="top"><i class="bx bx-x" onclick="core.closeWindow(this);" style="margin-right: 6px; background-color: #ff0000; color: white; border-radius: 30px; cursor: pointer;"></i>Test</div><iframe src="' + app + '"></iframe></div>');
+    $('#desktop').append('<div class="window" style="width: 300px; top: 75px; left: 85px;" onclick="core.focusWindow(this);"><div class="top"><i class="bx bx-x" onclick="core.closeWindow(this);" style="margin-right: 6px; background-color: #ff0000; color: white; border-radius: 30px; cursor: pointer;"></i>Test</div><iframe src="' + app + '"></iframe></div>');
     let position = { x: 0, y: 0 }
 
     interact('.window').draggable({
@@ -74,7 +74,7 @@ var core = {
   },
   focusWindow: function(el) {
     el.style.zIndex = 5;
-    $('.window').not('#' + el.id).css('z-index', 5);
+    $('.window').not(el).css('z-index', 5);
   }
 };
 
