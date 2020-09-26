e2d17e21654ac1bc66593523fc22bd441c69f159
liimee <70614549+liimee@users.noreply.github.com>
2020-09-26 17:14:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log('%cWebDesktop WIP', 'padding: 8px; background-color: #007bff; color: white; border: 2px solid #00a1ff; font-size: 35px;');
+
 var core = {
   root: document.documentElement,
   homeOpen: false,
