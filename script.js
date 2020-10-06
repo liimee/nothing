a99f3174d6efd660e3acb3a2274aaa7e455a8272
liimee <70614549+liimee@users.noreply.github.com>
2020-10-06 11:52:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -264,7 +264,7 @@ core.clockTooltip = tippy(document.querySelector('#bar #clock'), { trigger: 'cli
 core.deviceTooltip = tippy(document.querySelector('#bar #device'), {trigger: 'click', arrow: false, content: navigator.appVersion });
 core.fpsTooltip = tippy(document.querySelector('#bar #fps'), { trigger: 'click', arrow: false });
 core.networkTooltip = tippy(document.querySelector('#bar #network'), {trigger: 'click', arrow: false});
-core.powerOffBtnTooltip = tippy(document.querySelector('#power-off-button'), { trigger: 'click', interactive: true, arrow: false, allowHTML: true, content: '<span onclick="core.powerOff();">Power Off?</span>' });
+core.powerOffBtnTooltip = tippy(document.querySelector('#power-off-button'), { trigger: 'click', interactive: true, arrow: false, allowHTML: true, content: '<span onclick="core.powerOff();" style="background-color: #ff4242; color: white; width: 100%; padding: 8px;">Power Off?</span>' });
 
 if (navigator.connection) {
   networkThing();
