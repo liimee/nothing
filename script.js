bc17d5107c84aac7ca4e9a0dc8612eef5729d4b0
liimee <70614549+liimee@users.noreply.github.com>
2020-10-06 22:50:20 +0700

    test

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ var core = {
     document.querySelector('#overlay').style.opacity = 0.85;
     document.querySelector('#home').style.transform = 'scale(0, 0)';
     document.querySelector('#home').title = 'Close';
+    document.querySelector('#bar #power-off-button, #bar #clock, #bar #fps, #bar #network, #bar device').style.opacity = 0;
+    document.querySelector('#overlay #top').appendChild(document.querySelector('#bar #power-off-button, #bar #clock, #bar #fps, #bar #network, #bar device'));
+    document.querySelector('#bar #power-off-button, #bar #clock, #bar #fps, #bar #network, #bar device').style.opacity = 1;
     $('#home').attr('onclick', 'core.closeHome();');
     core.homeOpen = true;
     setTimeout(function() {
