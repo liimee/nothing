71272cb1ebddb368453e4b5bfe11627e46306cd9
liimee <70614549+liimee@users.noreply.github.com>
2020-10-08 23:22:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var core = {
   openApp: function(app) {
     core.closeHome();
     $('.window').css('z-index', 3);
-    let thatNewWindow = $('#desktop').append('<div class="window ' + app.id + '" data-maximized="false" style="width: 450px; top: 75px; left: 85px; height: 310px; z-index: 4;" onclick="core.bringWindowToFront(this);"><div class="top" ondblclick="core.maximizeWindow(this.parentElement);"><i class="bx bx-x" onclick="core.closeWindow(this);" style="margin-right: 6px; background-color: #ff0000; color: white; border-radius: 30px; cursor: pointer;"></i><i class="bx bx-expand" style="margin-right: 5px; border-radius: 30px; cursor: pointer; color: white; background-color: #5cce00;" onclick="core.maximizeWindow(this.parentElement.parentElement);"></i>' + app.name + '</div><div style="padding-right: 10px; padding-left: 10px; height: 85%;"><iframe src="' + app.file + '"></iframe></div></div>');
+    $('#desktop').append('<div class="window ' + app.id + '" data-maximized="false" style="width: 450px; top: 75px; left: 85px; height: 310px; z-index: 4;" onclick="core.bringWindowToFront(this);"><div class="top" ondblclick="core.maximizeWindow(this.parentElement);"><i class="bx bx-x" onclick="core.closeWindow(this);" style="margin-right: 6px; background-color: #ff0000; color: white; border-radius: 30px; cursor: pointer;"></i><i class="bx bx-expand" style="margin-right: 5px; border-radius: 30px; cursor: pointer; color: white; background-color: #5cce00;" onclick="core.maximizeWindow(this.parentElement.parentElement);"></i>' + app.name + '</div><div style="padding-right: 10px; padding-left: 10px; height: 85%;"><iframe src="' + app.file + '"></iframe></div></div>');
     let position = { x: 0, y: 0 }
 
     interact('.window').draggable({
@@ -47,8 +47,6 @@ var core = {
         move (event) {
           position.x += event.dx
           position.y += event.dy
-
-          core.bringWindowToFront(thatNewWindow);
 
           event.target.style.transform =
             `translate(${position.x}px, ${position.y}px)`
