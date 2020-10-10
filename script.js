9755a7b74e2bfc9f1d9852ccf8901c7c2863cb9c
liimee <70614549+liimee@users.noreply.github.com>
2020-10-10 22:44:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ var core = {
   },
   receiveNotif: function(which, app) {
     let msgchannel = new MessageChannel();
-    channel.port1.onmessage = function (e) {
+    msgchannel.port1.onmessage = function (e) {
       document.querySelector('#notif #notifTitle').innerText = app.name;
       document.querySelector('#notif #notifContent').innerText = e.data;
       document.querySelector('#notif').top = '24px';
